87358990fcd2197d682bb210023d48b7fd0113f5
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-14 09:00:26 +0200

    fixing some errors

--- react-express-app/frontend/frontend/src/App.jsx
+++ react-express-app/frontend/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,33 +7,27 @@ import FirstComponent from './components/FirstComponent'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [messaggio, setMessaggio] = useState('Caricamento...');
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/saluto')
+      .then(response =>{ 
+        response.json(); 
+        console.log(response);
+      })
+      .catch(error => {
+        console.error('Errore nella chiamata API: ', error);
+        setMessaggio('Errore nel caricamento');
+      });
+  }, []);
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        <FirstComponent></FirstComponent>
-      </div>
-    </>
-  )
+    <div>
+      <h1>Messaggio dal backend: </h1>
+      <p>{messaggio}</p>
+    </div>
+  );
+  
 }
 
-export default App
+export default App;
