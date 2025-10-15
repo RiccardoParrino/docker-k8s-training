ea50e24a06cc093a6d75891125aa015c7bf5680a
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-15 07:19:23 +0200

    adding the apicall vue component

---
+++ vue-fastapi-app/frontend/vue-project/src/components/ApiCall.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Data from API:</h1>
+        <pre>{{ data }}</pre>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'; 
+
+export default {
+    name: 'ApiCall',
+    data() {
+        return {
+            data: null,
+            error: null
+        };
+    },
+    mounted() {
+        axios.get('http://localhost:8000/api/welcome')
+            .then( response => {
+                this.data = response.data;
+            } ) 
+            .catch ( error => {
+                this.error = error;
+                console.error('API error: ', error);
+            } );
+    }
+};
+
+</script>
